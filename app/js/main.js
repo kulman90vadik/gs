880308bb75016af768c260f9aa335f2621580b40
Vadim Kulman <vadim.kulman90@mail.ru>
2021-10-15 06:13:40 +0300

    add gallery, products, feedback, footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,27 @@
+$(function(){
+    // start menu
+    let menuLinks = document.querySelectorAll('.menu__dropdown-link');
+    let menuImages = document.querySelector('.menu__photo');
 
-// start menu
-let menuLinks = document.querySelectorAll('.menu__dropdown-link');
-let menuImages = document.querySelector('.menu__photo');
+    for(let i = 0; i < menuLinks.length; i++) {
+        menuLinks[i].addEventListener('mousemove', function(){
+            let menuLinksItem = menuLinks[i].getAttribute('data');
+            menuImages.setAttribute('src', menuLinksItem);
+        });
+    }
+    // finish menu
 
-for(let i = 0; i < menuLinks.length; i++) {
-    menuLinks[i].addEventListener('mousemove', function(){
-        let menuLinksItem = menuLinks[i].getAttribute('data');
-        menuImages.setAttribute('src', menuLinksItem);
+    $('.page__slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><svg class="icon"><use xlink:href="images/sprite.svg#arrow-left"></use></svg></button>',
+        nextArrow: '<button type="button" class="slick-next"><svg class="icon"><use xlink:href="images/sprite.svg#arrow-right"></use></svg></button>'
     });
-}
-// finish menu
 
+    $('.products__list').slick({
+        nextArrow: $('.products__next'),
+        prevArrow: $('.products__prev'),
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: false
+    });
+});
