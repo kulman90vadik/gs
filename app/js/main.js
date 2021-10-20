8bf23d98663689e0a6f1e735e6853eb096b4c304
Vadim Kulman <vadim.kulman90@mail.ru>
2021-10-20 20:43:07 +0300

    add registry

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,68 @@
 $(function(){
-    // start menu
+
+    // select 
+    let registryBtn = document.querySelector('.registry__btn');
+    let registryOptions = document.querySelector('.registry__options');
+    registryBtn.addEventListener('click', function(){
+        registryOptions.classList.toggle('registry__options--active');
+        document.querySelector('.registry__icon').classList.toggle('registry__icon--active');
+    });
+
+    let registryOptionsItems = document.querySelectorAll('.registry__option');
+    registryOptionsItems.forEach(elem => {
+        elem.addEventListener('click', function(){
+            registryOptions.classList.toggle('registry__options--active');
+            document.querySelector('.registry__icon').classList.toggle('registry__icon--active');
+            registryBtn.value = elem.getAttribute('data-value');
+            document.querySelector('.registry__text').textContent = elem.textContent;
+        });
+    });
+    // finish select
+
+
+
+    let sidebarHeading = document.querySelectorAll('.sidebar-filter__heading');
+    sidebarHeading.forEach(elem => {
+        elem.addEventListener('click', function(){
+            elem.classList.toggle('sidebar-filter__heading--active');
+            elem.querySelector('.sidebar-filter__icon').classList.toggle('sidebar-filter__icon--active');
+            let sidebarFilter = elem.closest('.sidebar-filter');
+            let sidebarHolder = sidebarFilter.querySelector('.sidebar-filter__holder');
+            sidebarHolder.classList.toggle('sidebar-filter__holder--active');
+            sidebarFilter.classList.toggle('sidebar-filter--active');
+        });
+    });
+
+
+    $('.sidebar-filter__range').ionRangeSlider({
+        onStart: function (data) {
+            $('.sidebar-filter__min-number').text(data.from);
+            $('.sidebar-filter__max-number').text(data.to);
+        },
+        onChange: function (data) {
+            $('.sidebar-filter__min-number').text(data.from);
+            $('.sidebar-filter__max-number').text(data.to);
+        }
+    });
+
+
+    let header = document.querySelector('.header');
+    let headerTop = document.querySelector('.header__top');
+    let main = document.querySelector('.main');
+
+    window.addEventListener('scroll', function(){
+        if(window.scrollY >= 116) {
+            headerTop.classList.add('header__top--hidden');
+            header.classList.add('header--active');
+            main.style.paddingTop = `${116}px`;
+        } else {
+            headerTop.classList.remove('header__top--hidden');
+            header.classList.remove('header--active');
+            main.style.paddingTop = 0;
+        }
+    });
+
+//     // start menu
     let menuLinks = document.querySelectorAll('.menu__dropdown-link');
     let menuImages = document.querySelector('.menu__photo');
 
@@ -9,7 +72,9 @@ $(function(){
             menuImages.setAttribute('src', menuLinksItem);
         });
     }
-    // finish menu
+//     // finish menu
+
+//     // sliders
 
     $('.page__slider').slick({
         dots: true,
@@ -24,4 +89,24 @@ $(function(){
         slidesToScroll: 1,
         infinite: false
     });
+
+//     // add cards category
+
+    let categoryLinkAll = document.querySelector('.category__link-all');
+    categoryLinkAll.addEventListener('click', function(e){
+        e.preventDefault();
+        let categoryItemsHidden = document.querySelectorAll('.category__item--hidden');
+        categoryItemsHidden.forEach(elem => {
+            elem.classList.add('category__item--show');
+        });
+        this.classList.add('category__link--remove')
+    });
+
+    // 
+
+  
+
+
+
 });
+
